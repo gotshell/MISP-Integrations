a7b1efc8161747b06d8871cce8dee0f039a273f1
gotshell <53199443+gotshell@users.noreply.github.com>
2024-01-02 14:43:06 +0100

    Create delete_old_events.py

---
+++ MISP/delete_old_events.py
@@ -0,0 +1,27 @@
+import urllib3
+from pymisp import ExpandedPyMISP
+from datetime import datetime
+from datetime import date
+from dateutil.relativedelta import relativedelta
+
+misp_url = 'https://yourMispDomain'
+misp_key = '' 
+misp_verifycert = True
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def old_event_eraser(misp, delete_from):
+    events = misp.search(org='')                                                           # <-------- Search by org/tag etc.
+    for event in events:
+        last_change_timestamp = datetime.fromtimestamp(int(event['Event']['timestamp']))
+        event_info = event['Event']['info']
+        if last_change_timestamp.date() < delete_from:
+            misp.delete_event(event)
+            print(event_info)
+            print('Deleted')
+
+if __name__ == '__main__' :
+    misp = ExpandedPyMISP(misp_url, misp_key, misp_verifycert)
+    today = date.today()
+    one_month_ago = today - relativedelta(months=1)                                         # <------- It will delete events older than 1 month
+    old_event_eraser(misp, one_month_ago)
