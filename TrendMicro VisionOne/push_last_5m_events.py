fa74782d1833003e6eecba07b4a26286c63f527e
gotshell <53199443+gotshell@users.noreply.github.com>
2024-03-18 11:58:50 +0100

    Create push_last_5m_events.py

---
+++ TrendMicro VisionOne/push_last_5m_events.py
@@ -0,0 +1,159 @@
+# Importing libraries
+from pymisp import PyMISP
+import requests
+import urllib3
+import time
+from colorama import init as colorama_init
+from colorama import Fore
+from colorama import Style
+import validators
+import ipaddress
+import logging
+
+
+colorama_init()
+
+# ==============================================================================================================
+# 
+# TODO : CHECK FOR REFERENCE SET BEFORE TRYING TO PUSH SOMETHING IN IT
+# TODO : ADD EXPIRATION DATE TO IOCS PUSHED TO QRADAR
+# 
+# ==============================================================================================================
+
+
+# ---------------------------------------------- MISP API Settings ----------------------------------------------
+
+misp_url = 'https://'
+misp_key = ''
+misp_verifycert = True
+
+# --------------------------------------------- Qradar API Settings ---------------------------------------------
+
+qradar_url = 'https://_____________/api/reference_data/sets/bulk_load/'
+qradar_key = ''
+
+# -------------------------------Trend Micro VisionOne API configuration----------------------------------
+
+url_base = 'https://api.eu.xdr.trendmicro.com/'
+url_path = '/v3.0/threatintel/suspiciousObjects'
+token = ''
+
+query_params = {}
+headers = {
+    'Authorization': 'Bearer ' + token,
+    'Content-Type': 'application/json;charset=utf-8'
+}
+
+#---------------------------------------------GLOBAL VARIABLES----------------------------------------------
+DAYS_TO_EXPIRATION = '30'
+RISK_LEVEL = 'high'
+SCAN_ACTION = 'block'
+QRADAR_IP_REFERENCE_SET = 'qradar_reference_set_name_IP'
+QRADAR_URL_REFERENCE_SET = 'qradar_reference_set_name_URL'
+LAST_MINUTES = '5m'
+#----------------------------------------------------------------------------------------------------------
+
+
+# Disabling certificate warnings
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def search_and_create_set(orgs):
+    misp = PyMISP(misp_url, misp_key, misp_verifycert)
+    for organization in orgs:
+        url_set, domain_set, ip_set, email_set, sha1_set, sha256_set = set(), set(), set(), set(), set(), set()
+        qradar_url_list, qradar_ip_list = [], []
+        events = misp.search(controller='events', org=organization, to_ids=1, last=LAST_MINUTES, pythonify=True)
+        for event in events:
+            attributes, event_info = event.attributes, event.info
+            print(event_info)
+            for attribute in attributes:
+                att_value = attribute.value
+                att_type = check_type(att_value)
+                if att_type == 'url': 
+                    url_set.add(att_value)
+                    qradar_url_list.append(att_value)
+                elif att_type == 'domain': 
+                    domain_set.add(att_value)
+                    qradar_url_list.append(att_value)
+                elif att_type == 'Public IPv4': 
+                    ip_set.add(att_value)
+                    qradar_ip_list.append(att_value)
+                elif att_type == 'file_sha1': sha1_set.add(att_value)
+                elif att_type == 'SHA-256': sha256_set.add(att_value)
+
+        if url_set: push_to_tm_vision_one(url_set, 'url', f'{organization}\'s MISP event - {event_info}')      
+        if domain_set: push_to_tm_vision_one(domain_set, 'domain', f'{organization}\'s MISP event - {event_info}')          
+        if ip_set: push_to_tm_vision_one(ip_set, 'ip', f'{organization}\'s MISP event - {event_info}')
+        if sha1_set: push_to_tm_vision_one(sha1_set, 'fileSha1', f'{organization}\'s MISP event - {event_info}')
+        if sha256_set: push_to_tm_vision_one(sha256_set, 'fileSha256', f'{organization}\'s MISP event - {event_info}')
+        if qradar_ip_list: push_to_qradar(qradar_ip_list, QRADAR_IP_REFERENCE_SET)
+        if qradar_url_list: push_to_qradar(qradar_url_list, QRADAR_URL_REFERENCE_SET)
+
+
+def push_to_qradar(iocs_list, list_name):
+    headers = {
+    'Content-Type': 'application/json',
+    'SEC': qradar_key
+    }
+    qradar_url_ = qradar_url+list_name
+    try:
+        response = requests.post(qradar_url_, headers=headers, json=iocs_list, verify=False)
+        if response.status_code == 200:
+            print(f'{Fore.GREEN}IOCs successfully sent to QRadar.{Style.RESET_ALL}')   
+    except requests.HTTPError as e:
+        print('HTTPError: ', e)
+    except requests.exceptions.ConnectionError as e:
+        print('Connection-Error: ', e)
+        print('Retrying the request in 30 seconds...')
+        time.sleep(30)
+        response = requests.post(qradar_url_, headers=headers, json=iocs_list, verify=False)
+    except Exception as e:
+        print('2Generic Error: ', e)
+    time.sleep(3) 
+
+def push_to_tm_vision_one(bad_ioc, ioc_type, event_name):
+    body = []
+    for ioc in bad_ioc:
+        body.append({
+                f"{ioc_type}": f"{ioc}",
+                'description': f"{event_name}",
+                'scanAction': SCAN_ACTION,
+                'riskLevel': RISK_LEVEL,
+                'daysToExpiration': DAYS_TO_EXPIRATION
+        })
+
+    r = requests.post(url_base + url_path, params=query_params, headers=headers, json=body)
+    time.sleep(0.3)
+    if r.status_code == 207:
+        print('----------------------------------------------------------------------------------')
+        print('IOC sent to VisionOne instance')
+
+
+def is_public_ipv4(ip):
+    try:
+        ip_obj = ipaddress.ip_address(ip)
+        return not ip_obj.is_private
+    except ValueError:
+        return False
+
+def check_type(data):
+    if validators.url(data): return "url"
+    elif validators.domain(data): return "domain"
+    try:
+        ip = ipaddress.ip_address(data)
+        if ip.version == 4:
+            if is_public_ipv4(ip): return "Public IPv4"
+            else: return "Private IPv4"
+        elif ip.version == 6: return "IPv6"
+    except ValueError:
+        pass
+        # Check hash types
+    if len(data) == 40 and all(c in '0123456789abcdefABCDEF' for c in data): return "file_sha1"
+    elif len(data) == 32 and all(c in '0123456789abcdefABCDEF' for c in data): return "MD5"
+    elif len(data) == 64 and all(c in '0123456789abcdefABCDEF' for c in data): return "SHA-256"
+    return "Not recognised"
+
+
+if __name__ == '__main__' :
+    org_names_list = ['SIAG',  'CERT-AGID_6929'] #'Lepida', 'Pasubio Tecnologia', 'Trentino Digitale',
+    search_and_create_set(org_names_list)
